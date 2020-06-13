49e2e6eac518331f9a9696ebcd78d360cc723f0e
Prachi Singh <47474195+Prach1810@users.noreply.github.com>
2020-06-13 06:04:18 +0530

    Add files via upload

--- Tracker/home/urls.py
+++ Tracker/home/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-from home.views import home_view, home_form
+from home.views import home_view, home_form,home, facts
 
 urlpatterns = [
-    url(r'^$', home_form, name="home_form"),
+    #url(r'^$', home_form, name="home_form"),
+    url(r'^$', home, name="home"),
+    url(r'^form$', home_form, name="home_form"),
     url(r'^result$', home_view, name="home_view"),
+    url(r'^facts$', facts, name="facts"),
+
 ]
--- Tracker/home/views.py
+++ Tracker/home/views.py
@@ -2,11 +2,17 @@ import pandas as pd
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 import joblib
 
-def home_form(request):
+def home(request):
     if request.user.is_authenticated:
-        return render(request, "home_form.html")
+        return render(request, "home.html")
     else:
         return redirect(reverse('login'))
+
+def home_form(request):
+        return render(request, "home_form.html")
+
+def facts(request):
+    return render(request, "facts.html")
 
 def home_view(request):
     if request.user.is_authenticated:
@@ -17,24 +23,74 @@ def home_view(request):
         #country = request.POST['country']
         #sex = int(request.POST['sex'])
         diffBreathe = int(request.POST['diffBreathe'])
+        #sore = int(request.POST['sore'])
         bodyPain = int(request.POST['bodyPain'])
         runnyNose = int(request.POST['runnyNose'])
+        #nasal = int(request.POST['nasal'])
+        #diarrhea = int(request.POST['diarrhea'])
+        #tired = int(request.POST['tired'])
         fever = int(request.POST['fever'])
+        #dry = float(request.POST['dry'])
+        #contact = request.POST['contact']
+        #severe = request.POST['severe']
+        """
+        age = request.POST['age']
+        country = request.POST['country']
+        sex = int(request.POST['sex'])
+        breathe = int(request.POST['breathe'])
+        sore = int(request.POST['sore'])
+        pain = int(request.POST['pain'])
+        nose = int(request.POST['nose'])
+        nasal = int(request.POST['nasal'])
+        diarrhea = int(request.POST['diarrhea'])
+        tired = int(request.POST['tired'])
+        fever = int(request.POST['fever'])
+        dry = float(request.POST['dry'])
+        contact = request.POST['contact']
+        severe = request.POST['severe']
         
+       
+        """
         lst = [fever,bodyPain,age, runnyNose, diffBreathe]
-        
+        #lst = [age, country,sex, breathe, sore, pain, nose, nasal, diarrhea, tired, fever, dry, contact, severe]
         print(lst)
         df =pd.DataFrame([lst])
         df.columns=['fever','bodyPain','age', 'runnyNose', 'diffBreathe']
-        
+        #df.columns =['age','country','sex',' breathe', 'sore', 'pain', 'nose', 'nasal', 'diarrhea', 'tired', 'fever', 'dry', 'contact', 'severe']
         result= loaded_model.predict_proba(df)[0][1]
-        
         print(result)
-        
-        df = pd.read_csv('data.csv')
-        
         context={"result":result}
-        return render(request, "home.html",context)
-        
+        df = pd.read_csv('data.csv')
+        if result <0.3:
+            return render(request, "mild.html",context)
+
+        elif result >=0.3 and result < 0.7:
+            return render(request, "moderate.html",context)
+
+        else:
+            return render(request, "severe.html",context)
+        """
+        age = list(df['age'])
+        age_zero = list(df.query('target==0')['age'])
+        age_one = list(df.query('target==1')['age'])
+        chol_zero = list(df.query('target==1')['chol'])
+        chol_one = list(df.query('target==1')['chol'])
+        chol = list(df['chol'])
+        target = list(df['target'])
+        target1 = target[0:100]
+        chol1 = chol[0:100]
+        return render(request, "home.html", {
+            "age":age,
+            "chol":chol,
+            "target":target,
+            "age_zero":age_zero,
+            "age_one":age_one,
+            "chol_zero":chol_zero,
+            "chol_one":chol_one,
+            "result":result[0],
+            "target1":target1,
+            "chol1":chol1
+        })
+        """
     else:
         return redirect(reverse('login'))
